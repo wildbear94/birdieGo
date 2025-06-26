e454813415cee296dde80c18662107e23dcc8e91
park sang young <wildbear94@gmail.com>
2025-06-26 16:19:51 +0900

    로그인 응답 수정

--- birdileGoApi/src/main/java/com/yeoni/birdilegoapi/controller/AuthController.java
+++ birdileGoApi/src/main/java/com/yeoni/birdilegoapi/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.yeoni.birdilegoapi.controller;
 
+import com.yeoni.birdilegoapi.domain.dto.CommonResponse;
 import com.yeoni.birdilegoapi.domain.dto.auth.LoginRequest;
 import com.yeoni.birdilegoapi.domain.dto.auth.LoginResponse;
 import com.yeoni.birdilegoapi.domain.dto.auth.TokenResponse;
 import com.yeoni.birdilegoapi.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,10 +21,18 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<CommonResponse<LoginResponse>> login(@RequestBody LoginRequest loginRequest) {
 
         LoginResponse loginResponse = authService.login(loginRequest.getLoginId(), loginRequest.getPassword());
-        return ResponseEntity.ok(loginResponse);
+
+        CommonResponse<LoginResponse> response = CommonResponse.<LoginResponse>builder()
+            .status(HttpStatus.OK.value())
+            .code("SUCCESS")
+            .message("로그인에 성공했습니다.")
+            .data(loginResponse)
+            .build();
+
+        return ResponseEntity.ok(response);
 
     }
 
